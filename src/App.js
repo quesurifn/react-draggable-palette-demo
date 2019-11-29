4ca5de6acadc2a34eb6573194ecd6ffe188d21a1
quesurifn <kyle.c.r.fahey@gmail.com>
2019-11-28 20:00:36 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Draggable } from 'react-smooth-dnd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class SimpleSortableList extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      array: [0,1,2,3,4]
+    }
+
+    this.onDrop = this.onDrop.bind(this)
+  }
+  
+ 
+  dragInArray(array, added, removed) {
+    const removedCopy = array[removed]
+    array.splice(removed, 1);
+    array.splice(added, 0, removedCopy);
+    return array;
+  }
+
+  onDrop(e) {
+    let stateCopy = this.state.array
+    const swapped = this.dragInArray(stateCopy, e.addedIndex, e.removedIndex)
+    this.setState({array: swapped})
+  }
+
+  render() {
+    return (
+      <div>
+        <Container orientation="horizontal" onDrop={(e) => this.onDrop(e)}>
+
+          {this.state.array.map(item => {
+            return (
+              <Draggable key={item}>
+                <div style={{height: "100vh", width: "20vw", border: "solid 4px black"}}>
+                    {item}
+                </div>
+              </Draggable>
+            );
+          })}
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default SimpleSortableList;
